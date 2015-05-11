f83f54f93919727b2424df50eebb67669dfc63e2
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-05-11 15:44:20 +0100

    edited user routes file

--- routes/user.route.js
+++ routes/user.route.js
@@ -12,40 +12,14 @@ module.exports = function(app){
 
   userRouter.post('/authenticate', user.authenticateUser);
 
-  // //middleware in charge of all user requests
-  // userRouter.use(function(req, res, next){
-  //   //check post parameters for token
-  //   var token = req.headers['x-access-token'];
-
-  //   //if there is a token, decode it
-  //   if(token){
-  //     jwt.verify(token, superSecret, function(err, decoded){
-  //       if(err){
-  //         res.json({
-  //           message: 'Token could not be authenticated'
-  //         });
-  //       }
-  //       else{
-  //         req.decoded = decoded;
-  //         //console.log(req.decoded);
-  //         next();
-  //       }
-  //     });
-  //   }else{
-  //     return res.status(403).json({
-  //       message: 'Token not found'
-  //     });
-  //   }
-  // });
-
   userRouter.route('/users')
-    .get(user.verifyToken, user.getUsers)
-    .post(user.addUser);
+    .get(user.getUsers)
+    .post(user.verifyToken, user.addUser);
 
   userRouter.route('/user/:user_id')
     .get(user.getOneUser)
-    .put(user.updateUser)
-    .delete(user.deleteUser);
+    .put(user.verifyToken, user.updateUser)
+    .delete(user.verifyToken, user.deleteUser);
 
   userRouter.get('/me', function(req, res){
     res.send(req.decoded);
